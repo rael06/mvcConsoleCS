605ebd790e42d46b3fead019da427f3a0a7a211d
rael <rael06@hotmail.fr>
2020-01-22 21:21:29 +0100

    updated

--- Program.cs
+++ Program.cs
@@ -7,7 +7,6 @@ namespace mvcConsoleCS
 		static void Main(string[] args)
 		{
 			var paramsNames = new string[] { "controller", "ip", "port" };
-			//var app = new AppCore(paramsNames, args);
 			var app = new App(paramsNames, args);
 
 			app.Route();
